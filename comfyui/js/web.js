b022984228989749b8b4b2b29bd28f276af6ea3c
mjsh34 <mjsh25@yonsei.ac.kr>
2024-02-23 01:21:27 +0900

    A&E grad fails

--- comfyui/js/web.js
+++ comfyui/js/web.js
@@ -10,6 +10,9 @@ app.registerExtension({
             nodeData.input.required.load_config_yaml = 
                 ["LOAD_CONFIG_YAML", nodeData.input.required.config[1].model_name];
         }
+        if (nodeData?.input?.required?.nouns_list?.[1]?.extracted_nouns_json) {
+            nodeData.input.required.extract_nouns = ["EXTRACT_NOUNS"];
+        }
     },
 
     getCustomWidgets(app) {
@@ -26,12 +29,6 @@ app.registerExtension({
                         return cb.apply(this, arguments);
                 };
 
-                //const loadConfigBtn = node.addWidget("button", null, null, () => {
-                //    loadConfigYaml();
-                //});
-                //loadConfigBtn.label = "load config";
-                //loadConfigBtn.serialize = false;
-
                 async function loadConfigYaml() {
                     const body = new FormData();
                     const confFilename = configWidget.value;
@@ -45,7 +42,7 @@ app.registerExtension({
                     if (resp.status === 200) {
                         const data = await resp.json();
                         const conf = data.config;
-                        console.log(`Configuration '${confFilename}' successfully loaded:\n${conf}`);
+                        console.log(`Configuration '${confFilename}' successfully loaded:\n${JSON.stringify(conf)}`);
 
                         // Fill widget values
                         Object.entries(conf ?? {})
@@ -54,14 +51,14 @@ app.registerExtension({
                                 v: pVal,
                             }))
                             .filter(_ => _.w)
-                            .forEach(_ => _.w.value = _.v);
+                            .forEach(_ => { _.w.value = _.v; });
                         Object.entries(conf.params ?? {})
                             .map(([pName, pVal]) => ({
                                 w: node.widgets.find(w => w.name === `param_${pName}`),
                                 v: pVal,
                             }))
                             .filter(_ => _.w)
-                            .forEach(_ => _.w.value = _.v);
+                            .forEach(_ => { _.w.value = _.v; });
                     } else {
                         let msg = `Failed to load config: '${confFilename}'`;
                         const serverMsg = await resp.text();
@@ -70,6 +67,57 @@ app.registerExtension({
                         alert(msg);
                     }
                 };
+
+                const loadConfigWidget = node.addWidget("button", inputName, null, () => {
+                    loadConfigYaml();
+                });
+                loadConfigWidget.label = "load config yaml";
+                loadConfigWidget.serialize = false;
+
+                return { widget: loadConfigWidget };
+            },
+
+            EXTRACT_NOUNS(node, inputName, inputData, app) {
+                //const cb = node.callback;
+                const promptWidget = node.widgets.find(w => w.name === "prompt");
+                //promptWidget.callback = () => {
+                //    extractNouns();
+                //    if (cb)
+                //        return cb.apply(this, arguments);
+                //};
+
+                async function extractNouns() {
+                    const body = new FormData();
+                    const promptVal = promptWidget.value;
+                    body.append("prompt", promptVal);
+                    const resp = await api.fetchApi("/concept-guidance/extract-nouns", {
+                        method: "POST",
+                        body,
+                    });
+
+                    if (resp.status === 200) {
+                        const data = await resp.json();
+                        console.log(`Received nouns: ${data.nouns}`);
+                        const nounsListAsStr = JSON.stringify(data.nouns, null, 2)
+
+                        const nounsListWidget = node.widgets.find(w => w.name === "nouns_list");
+                        nounsListWidget.value = nounsListAsStr;
+                    } else {
+                        let msg = `Failed to extract nouns from: '${promptVal}'`;
+                        const serverMsg = await resp.text();
+                        if (serverMsg)
+                            msg += ` (${serverMsg})`;
+                        alert(msg);
+                    }
+                };
+
+                const extractNounsWidget = node.addWidget("button", inputName, null, () => {
+                    extractNouns();
+                });
+                extractNounsWidget.label = "extract nouns";
+                extractNounsWidget.serialize = false;
+
+                return { widget: extractNounsWidget };
             },
         }
     },
--- comfyui/nodes.py
+++ comfyui/nodes.py
@@ -1,33 +1,43 @@
-from .common import CONFIGS_DIR
+from .common import CONFIGS_DIR, extract_nouns, nlp_for_extract_nouns
 from ..pipelines import model_dict
+from ..utils.ptp_util import AttentionStore, register_attention_control
 
 import torch
 from torchvision.transforms import ToTensor
 import numpy as np
 import spacy
+from PIL import Image
 
 import os
 import os.path as osp
 from pathlib import Path
 from abc import ABC, abstractmethod
 import random
+import json
+import typing
 
 
 SCHEDULERS = ("ddim", "pndm", "ddpm")
 
 PIPELINE_TYPENAME = "DIFFUSERS_PIPELINE"
 PARAMSDICT_TYPENAME = "SAMPLER_PARAMS"
+# Attend and Excite
+ATTENTIONSTORE_TYPENAME = "ATTENTION_STORE"
 
 CATEGORY_BASE = "Concept Guidance"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device: {}".format(device))
 
+# This must match 'model' in model config yaml
 MODEL_NAME_STABLE_DIFFUSION = "stable_diffusion"
 MODEL_NAME_COMPOSABLE_DIFFUSION = "composable_diffusion"
+MODEL_NAME_ATTEND_AND_EXCITE = "attend_and_excite"
 
 MODEL_DISPLAY_NAME_STABLE_DIFFUSION = "Stable Diffusion"
 MODEL_DISPLAY_NAME_COMPOSABLE_DIFFUSION = "Composable Diffusion"
+MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE = "Attend and Excite"
+
 
 class BuildPipelineBase(ABC):
     @classmethod
@@ -43,6 +53,7 @@ class BuildPipelineBase(ABC):
             "version": ("STRING", {"default": ""}),
             "scheduler": (SCHEDULERS, ),
         }
+
     @classmethod
     def INPUT_TYPES(clazz):
         return {
@@ -74,14 +85,9 @@ class BuildPipelineBase(ABC):
         return True
 
     @classmethod
-    def get_node_identifier(clazz):
-        return clazz.__name__
-
-    @classmethod
     def get_node_display_name(clazz):
         return "Build Pipeline ({})".format(
-            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None
-            else clazz.get_node_identifier())
+            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None else clazz.MODEL_NAME)
 
 
 class LoadParamsBase(ABC):
@@ -135,21 +141,16 @@ class LoadParamsBase(ABC):
         return {k[len("param_"):]: v for k, v in d.items() if k.startswith("param_")}
 
     @classmethod
-    def get_node_identifier(clazz):
-        return clazz.__name__
-
-    @classmethod
     def get_node_display_name(clazz):
         return "Load Params ({})".format(
-            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None
-            else clazz.get_node_identifier())
+            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None else clazz.MODEL_NAME)
 
 
 class GenerateImageBase(ABC):
     REQUIRED_INPUT_TYPES = {
         "pipeline": (PIPELINE_TYPENAME, {}),
         "sampler_params": (PARAMSDICT_TYPENAME, {}),
-        "prompt": ("STRING", {"default": ""}),
+        "prompt": ("STRING", {"multiline": True}),
         "seed": ("INT", {"default": -1}),
     }
 
@@ -171,9 +172,15 @@ class GenerateImageBase(ABC):
         if not self.MODEL_NAME or self.MODEL_NAME != sampler_params.get('model_name', None):
             raise ValueError("model name mismatch ('{}':'{}')".format(
                 self.MODEL_NAME, sampler_params.get('model_name', None)))
-        return (self._images_to_tensors(pipeline(
+        images = self.get_images(
+                pipeline, sampler_params=sampler_params, prompt=prompt, seed=seed, **opts)
+        return (self._images_to_tensors(images), )
+
+    def get_images(self, pipeline, sampler_params, prompt, seed, **opts) -> typing.List[Image.Image]:
+        return pipeline(
                    prompt=prompt,
-                   generator=self._g(seed), **sampler_params).images), )
+                   generator=self._g(seed), **sampler_params).images
+
 
     @classmethod
     def _images_to_tensors(clazz, images):
@@ -184,14 +191,9 @@ class GenerateImageBase(ABC):
         return torch.Generator(device).manual_seed(seed if seed >= 0 else random.randint())
 
     @classmethod
-    def get_node_identifier(clazz):
-        return clazz.__name__
-
-    @classmethod
     def get_node_display_name(clazz):
         return "Generate ({})".format(
-            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None
-            else clazz.get_node_identifier())
+            clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None else clazz.MODEL_NAME)
 
 
 class BuildPipeline_StableDiffusion(BuildPipelineBase):
@@ -214,19 +216,14 @@ class LoadParams_StableDiffusion(LoadParamsBase):
         }
 
 
+class GenerateImage_StableDiffusion(GenerateImageBase):
+    MODEL_NAME = MODEL_NAME_STABLE_DIFFUSION
+    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_STABLE_DIFFUSION
+
+
 class BuildPipeline_ComposableDiffusion(BuildPipelineBase):
     MODEL_NAME = MODEL_NAME_COMPOSABLE_DIFFUSION
     MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_COMPOSABLE_DIFFUSION
-
-    @classmethod
-    def INPUT_TYPES(clazz):
-        return {
-            "required": clazz._get_required_input_types(),
-            "optional": {
-                "param_eta": ("FLOAT", {"default": 0.0}),
-                "param_guidance_rescale": ("FLOAT", {"default": 0.0}),
-            }
-        }
 
 
 class LoadParams_ComposableDiffusion(LoadParamsBase):
@@ -242,16 +239,6 @@ class LoadParams_ComposableDiffusion(LoadParamsBase):
                 "param_guidance_rescale": ("FLOAT", {"default": 0.0}),
             }
         }
-
-
-class GenerateImage_StableDiffusion(GenerateImageBase):
-    MODEL_NAME = MODEL_NAME_STABLE_DIFFUSION
-    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_STABLE_DIFFUSION
-
-
-class GenerateImage_ComposableDiffusion(GenerateImageBase):
-    MODEL_NAME = MODEL_NAME_COMPOSABLE_DIFFUSION
-    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_COMPOSABLE_DIFFUSION
 
 
 class PreprocessPromptForComposable:
@@ -271,44 +258,182 @@ class PreprocessPromptForComposable:
 
     FUNCTION = "preprocess"
     def preprocess(self, raw_prompt):
-        doc = self.nlp(raw_prompt)
-        noun_phrases = [chunk.text for chunk in doc.noun_chunks]
-        print("OUT", "|".join(noun_phrases))
-        return ("|".join(noun_phrases), )
-
-    @classmethod
-    def get_node_identifier(clazz):
-        return clazz.__name__
+        if not raw_prompt:
+            return ("", )
+        else:
+            doc = self.nlp(raw_prompt)
+            noun_phrases = [chunk.text for chunk in doc.noun_chunks]
+            print("OUT", "|".join(noun_phrases))
+            return ("|".join(noun_phrases), )
 
     @classmethod
     def get_node_display_name(clazz):
         return "Preprocess prompt for Composable Diffusion"
 
 
+class GenerateImage_ComposableDiffusion(GenerateImageBase):
+    MODEL_NAME = MODEL_NAME_COMPOSABLE_DIFFUSION
+    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_COMPOSABLE_DIFFUSION
+
+
+class BuildPipeline_AttendAndExcite(BuildPipelineBase):
+    MODEL_NAME = MODEL_NAME_ATTEND_AND_EXCITE
+    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
+
+    RETURN_TYPES = (PIPELINE_TYPENAME, ATTENTIONSTORE_TYPENAME)
+    RETURN_NAMES = ("pipeline", "attention_store")
+
+    def build_pipeline_fun(self, config, name, version, scheduler, **opts):
+        pipe = model_dict[self.MODEL_NAME].from_pretrained(version).to(device)
+        attention_store = AttentionStore() 
+        register_attention_control(pipe, attention_store)
+
+        return (pipe, attention_store)
+
+
+class LoadParams_AttendAndExcite(LoadParamsBase):
+    MODEL_NAME = MODEL_NAME_ATTEND_AND_EXCITE
+    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
+
+    @classmethod
+    def INPUT_TYPES(clazz):
+        return {
+            "required": {
+                **clazz._get_required_input_types(),
+                "param_attention_res": ("INT", {"default": 16}),
+                "param_max_iter_to_alter": ("INT", {"default": 25}),
+                "param_sigma": ("FLOAT", {"default": 0.5}),
+                "param_kernel_size": ("INT", {"default": 3}),
+                "param_smooth_attentions": ("BOOLEAN", {"default": True}),
+                "param_thresholds": ("STRING", {"default": json.dumps({0: 0.05, 10: 0.5, 20: 0.8}), "multiline": True}),
+                "param_scale_factor": ("INT", {"default": 20}),
+            },
+            "optional": {
+                "param_eta": ("FLOAT", {"default": 0.0}),
+                "param_guidance_rescale": ("FLOAT", {"default": 0.0}),
+            }
+        }
+
+
+class ExtractNounsForAttendAndExcite:
+    def __init__(self):
+        self.nlp = None
+
+    @classmethod
+    def INPUT_TYPES(clazz):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "do_extract_nouns_on_exec": ("BOOLEAN", {"default": True}),
+                "nouns_list": ("STRING", {
+                        "extracted_nouns_json": True,
+                        "multiline": True,
+                    }),
+            }
+        }
+
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("nouns_list", )
+
+    FUNCTION = "extract_nouns_fun"
+    def extract_nouns_fun(self, prompt, do_extract_nouns_on_exec: bool, nouns_list: str):
+        nouns = None # List[str]
+        if do_extract_nouns_on_exec:
+            if self.nlp is None:
+                self.nlp = nlp_for_extract_nouns
+            nouns = extract_nouns(prompt or "", nlp=self.nlp)
+        else:
+            nouns = json.loads(nouns_list)
+
+        nouns = list(map(str, nouns))
+        return (json.dumps(nouns), )
+
+    @classmethod
+    def VALIDATE_INPUTS(clazz, prompt, do_extract_nouns_on_exec: bool, nouns_list: str):
+        if not do_extract_nouns_on_exec:
+            try:
+                json.loads(nouns_list)
+            except JSONDecodeError:
+                return "Given nouns_list '{}' is not a proper JSON list.".format(nouns_list)
+        return True
+
+    @classmethod
+    def get_node_display_name(clazz):
+        return "Extract nouns"
+
+
+class GenerateImage_AttendAndExcite(GenerateImageBase):
+    MODEL_NAME = MODEL_NAME_ATTEND_AND_EXCITE
+    MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
+
+    @classmethod
+    def INPUT_TYPES(clazz):
+        return {
+            "required": {
+                **clazz.REQUIRED_INPUT_TYPES,
+                "attention_store": (ATTENTIONSTORE_TYPENAME, {"forceInput": True}),
+                "nouns": ("STRING", {"default": json.dumps([])}),
+            },
+        }
+
+    def get_images(self, pipeline, sampler_params, prompt, seed, **opts) -> typing.List[Image.Image]:
+        nouns = json.loads(opts['nouns'])
+        attention_store = opts['attention_store']
+        print("{} - nouns (#={}): {}".format(self.__class__.__name__, len(nouns), nouns))
+        token_indices = pipeline.get_indices(prompt)
+        indices_to_alter = []
+        for n in nouns:
+            try:
+                indices_to_alter.append(token_indices[n])
+            except:
+                continue
+
+        if 'thresholds' in sampler_params:
+            th_str = sampler_params['thresholds']
+            if not th_str.startswith("{"):  # Curly braces are stripped for some reason?
+                th_str = "{" + th_str + "}"
+            sampler_params['thresholds'] = json.loads(th_str)
+
+        return pipeline(
+                prompt=prompt,
+                attention_store=attention_store,
+                indices_to_alter=indices_to_alter,
+                generator=self._g(seed),
+                **{**sampler_params,
+                   'return_dict': True,
+                   'output_type': 'pil'}
+            ).images
+
+
 node_classes = [
         BuildPipeline_StableDiffusion,
         BuildPipeline_ComposableDiffusion,
+        BuildPipeline_AttendAndExcite,
 
         LoadParams_StableDiffusion,
         LoadParams_ComposableDiffusion,
+        LoadParams_AttendAndExcite,
 
         GenerateImage_StableDiffusion,
         GenerateImage_ComposableDiffusion,
+        GenerateImage_AttendAndExcite,
 
         PreprocessPromptForComposable,
+        ExtractNounsForAttendAndExcite,
 ]
 
-BuildPipeline_StableDiffusion.get_node_identifier()
+get_node_identifier = lambda clazz: clazz.__name__
+get_node_display_name = lambda clazz: getattr(clazz, "get_node_display_name")() \
+        if hasattr(clazz, "get_node_display_name") else get_node_identifier(clazz)
 
-NODE_CLASS_MAPPINGS = {clazz.get_node_identifier(): clazz for clazz in node_classes}
+NODE_CLASS_MAPPINGS = {get_node_identifier(clazz): clazz for clazz in node_classes}
 NODE_CLASS_MAPPINGS = {
     **NODE_CLASS_MAPPINGS,
     # ...
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = \
-        {clazz.get_node_identifier(): clazz.get_node_display_name()
-                for clazz in node_classes}
+        {get_node_identifier(clazz): get_node_display_name(clazz) for clazz in node_classes}
 NODE_DISPLAY_NAME_MAPPINGS = {
     **NODE_DISPLAY_NAME_MAPPINGS,
     # ...
