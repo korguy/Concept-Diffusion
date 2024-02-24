abeea5464a2b15a08ab7e7ec1d899737010b44af
mjsh34 <mjsh25@yonsei.ac.kr>
2024-02-25 01:35:16 +0900

    Pipelines can be automatically disposed after gen; some CUDA OOM bugs persist
    Using comfyui/workflows/workflow_sd_cd_ane.json you can generate for SD, CD, and A&E all at once without CUDA OOM with 24G VRAM.
    However, it will OOM the second time you run it and I suspect this is because for some reason, ComfyUI chooses to execute the nodes in a different order such that
    after generating the image for the first model another model's pipeline gets created and invoked, before DisposePipeline node for the first model can be executed following generation.

--- comfyui/nodes.py
+++ comfyui/nodes.py
@@ -15,14 +15,14 @@ from abc import ABC, abstractmethod
 import random
 import json
 import typing
+import gc
+import time
 
 
 SCHEDULERS = ("ddim", "pndm", "ddpm")
 
 PIPELINE_TYPENAME = "DIFFUSERS_PIPELINE"
 PARAMSDICT_TYPENAME = "SAMPLER_PARAMS"
-# Attend and Excite
-ATTENTIONSTORE_TYPENAME = "ATTENTION_STORE"
 
 CATEGORY_BASE = "Concept Guidance"
 
@@ -39,7 +39,39 @@ MODEL_DISPLAY_NAME_COMPOSABLE_DIFFUSION = "Composable Diffusion"
 MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE = "Attend and Excite"
 
 
+class PipelineDecorator:
+    def __init__(self, pipe, **extra):
+        self.pipe = pipe
+        self.extra = extra
+
+    def __call__(self, *a, **kw):
+        return self.pipe(*a, **kw)
+
+    def __del__(self):
+        self.dispose()
+
+    def dispose(self):
+        if hasattr(self, 'pipe'):
+            print(f"{self}: Deleting pipeline: {self.pipe}")
+            del self.pipe
+        self.pipe = None
+        for k, v in self.extra.items():
+            del v
+
+    def __getattr__(self, name):
+        if hasattr(self.pipe, name):
+            return getattr(self.pipe, name)
+        else:
+            raise AttributeError(name)
+
+
 class BuildPipelineBase(ABC):
+    #__do_refresh = True
+
+    def __init__(self):
+        self.__node_id = None
+        self.__pipeline_cached = None
+
     @classmethod
     def _get_required_input_types(clazz):
         if not clazz.MODEL_NAME:
@@ -58,6 +90,13 @@ class BuildPipelineBase(ABC):
     def INPUT_TYPES(clazz):
         return {
             "required": clazz._get_required_input_types(),
+            "optional": {
+                # If this is checked, executing this node will just del the cached pipeline and return nothing
+                "dispose_pipeline": ("BOOLEAN", {"default": False}),
+            },
+            "hidden": {
+                "node_id": "UNIQUE_ID",
+            },
         }
 
     MODEL_NAME = None  # Concrete subclasses MUST override this field
@@ -69,17 +108,54 @@ class BuildPipelineBase(ABC):
     RETURN_NAMES = ("pipeline", )
 
     def build_pipeline_fun(self, config, name, version, scheduler, **opts):
-        return (self.get_pipeline(config, name, version, scheduler, **opts), )
+        print("Building pipeline ({})".format(self.MODEL_DISPLAY_NAME))
+        self.dispose_pipeline()
+
+        if not opts['dispose_pipeline']:
+            self.__node_id = opts['node_id']
+            pipeline = self.get_pipeline(config, name, version, scheduler, **opts)
+            self.__pipeline_cached = pipeline
+            return (pipeline, )
+        else:
+            return (None, )
 
     def get_pipeline(self, config, name, version, scheduler, **opts):
-        return model_dict[self.MODEL_NAME].from_pretrained(version).to(device)
+        extra = {}
+        if node_id := opts.get('node_id'):
+            extra['node_id'] = node_id
+        extra['dispose_pipeline_fun'] = self.dispose_pipeline  # potential memory leak?
+        return PipelineDecorator(model_dict[self.MODEL_NAME].from_pretrained(version).to(device), **extra)
+
+    def dispose_pipeline(self):
+        if self.__pipeline_cached is not None:
+            print("Deleting pipeline: '{}'...".format(self.__pipeline_cached))
+            #del self.__pipeline_cached.pipe
+            #del self.__pipeline_cached
+            #self.__pipeline_cached.pipe = None
+            if hasattr(self.__pipeline_cached, 'dispose'):
+                self.__pipeline_cached.dispose()
+            else:
+                del self.__pipeline_cached
+            self.__pipeline_cached = None
+
+            gc.collect()
+            torch.cuda.empty_cache()
+        #gc.collect()
+        #torch.cuda.empty_cache()
+        #self.__class__.__do_refresh = True
 
     @classmethod
     def IS_CHANGED(clazz, config, name, version, scheduler, **opts):
-        return f"{config}_{name}_{version}_{scheduler}_{opts}"
+        #if clazz.__do_refresh or opts['dispose_pipeline']:
+        if self.__pipeline_cached is None or opts['dispose_pipeline']:
+            return float('nan')
+        else:
+            return f"{config}_{name}_{version}_{scheduler}_{opts}"
 
     @classmethod
     def VALIDATE_INPUTS(clazz, config, name, version, scheduler, **opts):
+        if not (name and version and scheduler):
+            return "Required fields missing"
         if not Path(CONFIGS_DIR, config).is_file():
             return "Invalid config: '{}'".format(config)
         return True
@@ -88,6 +164,9 @@ class BuildPipelineBase(ABC):
     def get_node_display_name(clazz):
         return "Build Pipeline ({})".format(
             clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None else clazz.MODEL_NAME)
+
+    def __del__(self):
+        self.dispose_pipeline()
 
 
 class LoadParamsBase(ABC):
@@ -122,6 +201,7 @@ class LoadParamsBase(ABC):
     RETURN_NAMES = ("sampler_params", )
 
     def load_params_fun(self, config, **opts):
+        print("Loading params ({})".format(self.MODEL_DISPLAY_NAME))
         return ({
             "model_name": self.MODEL_NAME,
             **self._get_params(opts)}, )
@@ -147,17 +227,19 @@ class LoadParamsBase(ABC):
 
 
 class GenerateImageBase(ABC):
-    REQUIRED_INPUT_TYPES = {
-        "pipeline": (PIPELINE_TYPENAME, {}),
-        "sampler_params": (PARAMSDICT_TYPENAME, {}),
-        "prompt": ("STRING", {"multiline": True}),
-        "seed": ("INT", {"default": -1}),
-    }
+    @classmethod
+    def _get_required_input_types(clazz):
+        return {
+            "pipeline": (PIPELINE_TYPENAME, {}),
+            "sampler_params": (PARAMSDICT_TYPENAME, {}),
+            "prompt": ("STRING", {"multiline": True}),
+            "seed": ("INT", {"default": -1}),
+        }
 
     @classmethod
     def INPUT_TYPES(clazz):
         return {
-            "required": clazz.REQUIRED_INPUT_TYPES,
+            "required": clazz._get_required_input_types(),
         }
 
     MODEL_NAME = None  # Concrete subclasses MUST override this field
@@ -165,22 +247,26 @@ class GenerateImageBase(ABC):
 
     CATEGORY = CATEGORY_BASE
     FUNCTION = "generate"
-    RETURN_TYPES = ("IMAGE", )
-    RETURN_NAMES = ("image", )
+    RETURN_TYPES = ("IMAGE", PIPELINE_TYPENAME, )
+    RETURN_NAMES = ("image", "pipeline", )
 
     def generate(self, pipeline, sampler_params, prompt, seed, **opts):
+        print("Generating image ({})".format(self.MODEL_DISPLAY_NAME))
         if not self.MODEL_NAME or self.MODEL_NAME != sampler_params.get('model_name', None):
             raise ValueError("model name mismatch ('{}':'{}')".format(
                 self.MODEL_NAME, sampler_params.get('model_name', None)))
+        if pipeline is None:
+            msg = "No pipeline given." + \
+                  " Make sure 'dispose_pipeline' in the Build pipeline node is unchecked."
+            raise ValueError(msg)
         images = self.get_images(
                 pipeline, sampler_params=sampler_params, prompt=prompt, seed=seed, **opts)
-        return (self._images_to_tensors(images), )
+        return (self._images_to_tensors(images), pipeline, )
 
     def get_images(self, pipeline, sampler_params, prompt, seed, **opts) -> typing.List[Image.Image]:
         return pipeline(
                    prompt=prompt,
                    generator=self._g(seed), **sampler_params).images
-
 
     @classmethod
     def _images_to_tensors(clazz, images):
@@ -194,6 +280,59 @@ class GenerateImageBase(ABC):
     def get_node_display_name(clazz):
         return "Generate ({})".format(
             clazz.MODEL_DISPLAY_NAME if clazz.MODEL_DISPLAY_NAME is not None else clazz.MODEL_NAME)
+
+    @classmethod
+    def IS_CHANGED(clazz, **kw):
+        return float('nan')
+
+
+class DisposePipeline:
+    @classmethod
+    def INPUT_TYPES(clazz):
+        return {
+            "required": {
+                "pipeline": (PIPELINE_TYPENAME, {}),
+            },
+            "optional": {
+                "after": ("*", {}),
+            },
+            "hidden": {
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
+        }
+
+    CATEGORY = CATEGORY_BASE
+    FUNCTION = "dispose_pipeline"
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    def dispose_pipeline(self, pipeline, after=None, extra_pnginfo=None):
+        print("Disposing pipeline")
+        # Can't seem to grab the reference to the python object this way
+        #if 'workflow' in extra_pnginfo:
+        #    workflow = extra_pnginfo['workflow']
+        #else:
+        #    # https://github.com/pythongosssss/ComfyUI-Custom-Scripts/blob/main/py/show_text.py
+        #    workflow = extra_pnginfo[0]['workflow']
+        #pipe_nid = pipeline.extra['node_id']
+
+        #pipeline_node = next((n for n in workflow['nodes'] if str(n['id']) == pipe_nid), None)
+        #if pipeline_node is None:
+        #    raise ValueError("Could not locate the pipeline node (id={})".format(pipe_nid))
+
+        pipeline.extra['dispose_pipeline_fun']()
+        #pipeline.dispose()
+        #del pipeline.extra['dispose_pipeline_fun']
+        #del pipeline
+        #gc.collect()
+        #torch.cuda.empty_cache()
+
+        return {"ui": {"text": "Removed pipeline"}}
+
+    @classmethod
+    def get_node_display_name(clazz):
+        return "Dispose Pipeline"
 
 
 class BuildPipeline_StableDiffusion(BuildPipelineBase):
@@ -280,16 +419,21 @@ class BuildPipeline_AttendAndExcite(BuildPipelineBase):
     MODEL_NAME = MODEL_NAME_ATTEND_AND_EXCITE
     MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
 
-    RETURN_TYPES = (PIPELINE_TYPENAME, ATTENTIONSTORE_TYPENAME)
-    RETURN_NAMES = ("pipeline", "attention_store")
 
-    def build_pipeline_fun(self, config, name, version, scheduler, **opts):
+    def get_pipeline(self, config, name, version, scheduler, **opts):
         with torch.inference_mode(False):
             pipe = model_dict[self.MODEL_NAME].from_pretrained(version).to(device)
         attention_store = AttentionStore() 
         register_attention_control(pipe, attention_store)
 
-        return (pipe, attention_store)
+        extra = {}
+        if node_id := opts.get('node_id'):
+            extra['node_id'] = node_id
+        extra['dispose_pipeline_fun'] = self.dispose_pipeline
+
+        extra['attention_store'] = attention_store
+
+        return PipelineDecorator(pipe, **extra)
 
 
 class LoadParams_AttendAndExcite(LoadParamsBase):
@@ -297,22 +441,19 @@ class LoadParams_AttendAndExcite(LoadParamsBase):
     MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
 
     @classmethod
-    def INPUT_TYPES(clazz):
+    def _get_required_input_types(clazz):
         return {
-            "required": {
-                **clazz._get_required_input_types(),
-                "param_attention_res": ("INT", {"default": 16}),
-                "param_max_iter_to_alter": ("INT", {"default": 25}),
-                "param_sigma": ("FLOAT", {"default": 0.5}),
-                "param_kernel_size": ("INT", {"default": 3}),
-                "param_smooth_attentions": ("BOOLEAN", {"default": True}),
-                "param_thresholds": ("STRING", {"default": json.dumps({0: 0.05, 10: 0.5, 20: 0.8}), "multiline": True}),
-                "param_scale_factor": ("INT", {"default": 20}),
-            },
-            "optional": {
-                "param_eta": ("FLOAT", {"default": 0.0}),
-                "param_guidance_rescale": ("FLOAT", {"default": 0.0}),
-            }
+            **super(clazz, clazz)._get_required_input_types(),
+            "param_attention_res": ("INT", {"default": 16}),
+            "param_max_iter_to_alter": ("INT", {"default": 25}),
+            "param_sigma": ("FLOAT", {"default": 0.5}),
+            "param_kernel_size": ("INT", {"default": 3}),
+            "param_smooth_attentions": ("BOOLEAN", {"default": True}),
+            "param_thresholds": ("STRING", {"default": json.dumps({0: 0.05, 10: 0.5, 20: 0.8}), "multiline": True}),
+            "param_scale_factor": ("INT", {"default": 20}),
+
+            "param_eta": ("FLOAT", {"default": 0.0}),
+            "param_guidance_rescale": ("FLOAT", {"default": 0.0}),
         }
 
 
@@ -368,18 +509,18 @@ class GenerateImage_AttendAndExcite(GenerateImageBase):
     MODEL_DISPLAY_NAME = MODEL_DISPLAY_NAME_ATTEND_AND_EXCITE
 
     @classmethod
-    def INPUT_TYPES(clazz):
+    def _get_required_input_types(clazz):
         return {
-            "required": {
-                **clazz.REQUIRED_INPUT_TYPES,
-                "attention_store": (ATTENTIONSTORE_TYPENAME, {"forceInput": True}),
-                "nouns": ("STRING", {"default": json.dumps([])}),
-            },
+            **super(clazz, clazz)._get_required_input_types(),
+            #"attention_store": (ATTENTIONSTORE_TYPENAME, {"forceInput": True}),
+            "nouns": ("STRING", {"default": json.dumps([])}),
         }
 
     def get_images(self, pipeline, sampler_params, prompt, seed, **opts) -> typing.List[Image.Image]:
+        if 'attention_store' not in pipeline.extra:
+            raise ValueError("Invalid A&E pipeline: attention store is missing")
         nouns = json.loads(opts['nouns'])
-        attention_store = opts['attention_store']
+        attention_store = pipeline.extra['attention_store']
         print("{} - nouns (#={}):  {}".format(self.__class__.__name__, len(nouns), nouns))
         token_indices = pipeline.get_indices(prompt)
         indices_to_alter = []
@@ -395,7 +536,6 @@ class GenerateImage_AttendAndExcite(GenerateImageBase):
             sampler_params['thresholds'] = {int(k): v for k, v in json.loads(th_str).items()}
 
         with torch.inference_mode(False):
-            pipeline.unet.train()
             images = pipeline(
                     prompt=prompt,
                     attention_store=attention_store,
@@ -423,6 +563,8 @@ node_classes = [
 
         PreprocessPromptForComposable,
         ExtractNounsForAttendAndExcite,
+
+        DisposePipeline,
 ]
 
 get_node_identifier = lambda clazz: clazz.__name__
